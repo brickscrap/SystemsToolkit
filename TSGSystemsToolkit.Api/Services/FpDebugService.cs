3cda4bf1bc908876ad4a22a69a1bf3157a415b9f
Gary MacGregor-Manzi <omgitsgary90@gmail.com>
2021-05-11 15:59:07 +0100

    Add gRPC
    Adds some sample gRPC services

---
+++ TSGSystemsToolkit.Api/Services/FpDebugService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using GrpcServer;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace TsgSystems.Api.Services
+{
+    public class FpDebugService : FpDebug.FpDebugBase
+    {
+        private readonly ILogger<FpDebugService> _logger;
+
+        public FpDebugService(ILogger<FpDebugService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<FpDebugReply> DebugFuelPOS(FpDebugRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("{StationId}, {Proccesses}", request.StationId, request.Processes);
+            System.Console.WriteLine(request.StationId + " " + request.Processes);
+
+            return Task.FromResult(new FpDebugReply { StatusCode = "200" });
+        }
+    }
+}
