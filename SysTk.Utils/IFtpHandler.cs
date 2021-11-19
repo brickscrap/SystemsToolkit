608d891bd9f81ca1f8f8b6f4268cc2f608ffedca
brickscrap <58906080+brickscrap@users.noreply.github.com>
2021-11-19 16:24:29 +0000

    Add FtpHandler

---
+++ SysTk.Utils/IFtpHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace SysTk.Utils
+{
+    public interface IFtpHandler
+    {
+        Task<bool> DownloadFileAsync(string host, string user, string password, string localPath, string remotePath, int port = 21, bool overwrite = false, bool deleteOnceDownloaded = false, CancellationToken ct = default);
+        Task<bool> UploadFileAsync(string host, string user, string password, string localPath, string remotePath, int port = 21, bool overwrite = false, bool deleteOnceUploaded = false, CancellationToken ct = default);
+    }
+}
