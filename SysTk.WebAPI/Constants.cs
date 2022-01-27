134d9c1b137a0d9c84c575c00ecf05ca68a55a5e
Gary MacGregor-Manzi <omgitsgary90@gmail.com>
2022-01-27 16:52:08 +0000

    Add policy and role constants

---
+++ SysTk.WebAPI/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace SysTk.WebAPI
+{
+    public static class Policies
+    {
+        public const string CanAdd = "CanAdd";
+        public const string CanDelete = "CanDelete";
+        public const string CanModifyUsers = "CanModifyUsers";
+        public const string IsVerified = "IsVerified";
+    }
+
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string Supervisor = "Supervisor";
+        public const string Member = "Member";
+    }
+}
