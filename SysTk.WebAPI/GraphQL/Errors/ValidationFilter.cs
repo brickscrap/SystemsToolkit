1c05ee4eeedac5ff43a1b716c824f5866d3fba28
Gary MacGregor-Manzi <omgitsgary90@gmail.com>
2022-01-27 16:50:23 +0000

    Add Validation

---
+++ SysTk.WebAPI/GraphQL/Errors/ValidationFilter.cs
@@ -0,0 +1,15 @@
+﻿namespace SysTk.WebAPI.GraphQL.Errors
+{
+    public class ValidationFilter : IErrorFilter
+    {
+        public IError OnError(IError error)
+        {
+            if (error.Code == "FairyBread_ValidationError")
+            {
+                error = error.RemoveExtensions().RemoveCode();
+            }
+
+            return error;
+        }
+    }
+}
