22fe1254a7fde1c3c08b4de35d4db14b12e3ac89
Gary MacGregor-Manzi <omgitsgary90@gmail.com>
2022-01-27 16:51:34 +0000

    Add validators

---
+++ SysTk.WebAPI/Validators/AddFtpCredentialsValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using SysTk.WebApi.Data.DataAccess;
+using SysTk.WebAPI.GraphQL.FtpCredential;
+
+namespace SysTk.WebAPI.Validators
+{
+    public class AddFtpCredentialsValidator : AbstractValidator<AddFtpCredentialsInput>
+    {
+
+        public AddFtpCredentialsValidator()
+        {
+            RuleFor(x => x.StationId).Length(5).WithMessage("Site ID must be exactly 5 characters.");
+        }
+    }
+}
---
+++ SysTk.WebAPI/Validators/AddStationInputValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using SysTk.WebApi.Data.DataAccess;
+using SysTk.WebAPI.GraphQL.Stations;
+
+namespace SysTk.WebAPI.Validators
+{
+    public class AddStationInputValidator : AbstractValidator<AddStationInput>
+    {
+        private readonly IDbContextFactory<AppDbContext> _contextFactory;
+
+        public AddStationInputValidator(IDbContextFactory<AppDbContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
+
+                RuleFor(x => x.Cluster).NotEmpty();
+                RuleFor(x => x.Name).NotEmpty();
+                RuleFor(x => x.Id).Length(5).WithMessage("Site ID must be exactly 5 characters.");
+                RuleFor(x => x.IP).Must(BeIPAddress).WithMessage("Invalid IP address provided.")
+                    .Must(IPNotExist).WithMessage("IP address already exists.");
+        }
+
+        private bool BeIPAddress(string ip) => IPAddress.TryParse(ip, out _);
+
+        private bool IPNotExist(string ip)
+        {
+            using var context = _contextFactory.CreateDbContext();
+
+            return !context.Stations.Where(x => x.IP == ip).Any();
+        }
+    }
+}
