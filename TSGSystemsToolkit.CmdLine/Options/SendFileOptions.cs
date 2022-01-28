78331b3366d6efb4575ca66374cb477595a31ce3
Gary MacGregor-Manzi <omgitsgary90@gmail.com>
2022-01-28 16:54:08 +0000

    Create options

---
+++ TSGSystemsToolkit.CmdLine/Options/SendFileOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSGSystemsToolkit.CmdLine.Options
+{
+    public class SendFileOptions : OptionsBase
+    {
+        public string FilePath { get; set; }
+        public string Output { get; set; }
+        public string Cluster { get; set; }
+        public string Target { get; set; }
+
+        public SendFileOptions(string filepath, string output, string? cluster, string target)
+        {
+            FilePath = filepath;
+            Output = output;
+            Cluster = cluster;
+            Target = target;
+        }
+    }
+}
