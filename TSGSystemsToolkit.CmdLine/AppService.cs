0b64e0400564c8e1bf43c30345d933b6f65808a0
Gary MacGregor-Manzi <omgitsgary90@gmail.com>
2022-01-28 16:54:32 +0000

    Inject Api Client

--- TSGSystemsToolkit.CmdLine/AppService.cs
+++ TSGSystemsToolkit.CmdLine/AppService.cs
@@ -7,6 +7,7 @@ using System.CommandLine.Help;
 using System.CommandLine.Rendering;
 using System.Threading.Tasks;
 using TSGSystemsToolkit.CmdLine.Commands;
+using TSGSystemsToolkit.CmdLine.GraphQL;
 
 namespace TSGSystemsToolkit.CmdLine
 {
@@ -15,16 +16,28 @@ namespace TSGSystemsToolkit.CmdLine
         private readonly ILogger<AppService> _logger;
         private readonly IRootCommands _rootCommands;
         private readonly IConfiguration _config;
+        private readonly SysTkApiClient _client;
 
-        public AppService(ILogger<AppService> logger, IRootCommands rootCommands, IConfiguration config)
+        public AppService(ILogger<AppService> logger, IRootCommands rootCommands, IConfiguration config, SysTkApiClient client)
         {
             _logger = logger;
             _rootCommands = rootCommands;
             _config = config;
+            _client = client;
         }
 
         public async Task<int> Run(string[] args)
         {
+            //Console.WriteLine("Connecting to client.");
+            
+            //var result = await _client.GetToken.ExecuteAsync("pow@use.r", "3A2o7z%$iE$6FxoRj6NBL8ZwVJ");
+            //if (result.Errors.Count == 0)
+            //    Extensions.UpdateAccessToken(result.Data.Login.AccessToken);
+            //else
+            //    Console.WriteLine("Error collecting token");
+            
+            //return 0;
+
             var cmd = _rootCommands.Create();
             cmd.Name = "SysTk";
             cmd.Description = "A series of command-line tools to help ease the burden of your left-click button.";
