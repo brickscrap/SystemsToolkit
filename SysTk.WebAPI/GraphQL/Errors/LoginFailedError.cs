fea154bc7068d10e074d93baa2ab3a62987ea017
Gary MacGregor-Manzi <omgitsgary90@gmail.com>
2022-03-01 12:46:29 +0000

    Remove error

--- SysTk.WebAPI/GraphQL/Errors/LoginFailedError.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SysTk.WebAPI.GraphQL.Errors
-{
-    public class LoginFailedError : Exception
-    {
-        public LoginFailedError() : base("Login failed. The username or password provided is not valid.")
-        {
-        }
-    }
-}
